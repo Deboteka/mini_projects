cc74d1f9f1705702a61c12541639e915a29443b7
libanos_kassahun <101578355+wajuskass@users.noreply.github.com>
2023-05-17 10:06:14 -0700

    Update script.js

--- project/quizapp/js/script.js
+++ project/quizapp/js/script.js
@@ -36,3 +36,30 @@ const questions =[
         ]
     }
 ];
+const questionElement= document.getElementById("question");
+const answerButtons= document.getElementById("answer-button");
+const nextButton= document.getElementById("next-btn");
+let currentQuestionIndex = 0;
+let score = 0;
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
